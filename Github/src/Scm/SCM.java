7992c4620242cec166d4e8b865c5177f58b6818c
biluraja <muhammadbilal11292@gmail.com>
2016-04-25 17:40:03 +0500

    first commit of lab

---
+++ Github/src/Scm/SCM.java
@@ -0,0 +1,14 @@
+package Scm;
+
+public class SCM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to github");
+		System.out.println("welcome to eclipse");
+		System.out.println("welcome to lab 7");
+		System.out.println("welcome to a block");
+
+	}
+
+}
